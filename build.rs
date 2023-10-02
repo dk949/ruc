f1ad2e6828e9b24a2342258ebfeb7ae6670ea370
dk949 <56653556+dk949@users.noreply.github.com>
2023-10-02 21:41:07 +0100

    removed extrainious comments from build.rs

--- build.rs
+++ build.rs
@@ -2,35 +2,6 @@ use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 use std::{env, fs};
-// HashMap::from([
-//     (LANGS![0], include_str!(concat!("snippets/", LANGS![0]))),
-//     (LANGS![1], include_str!(concat!("snippets/", LANGS![1]))),
-//     (LANGS![2], include_str!(concat!("snippets/", LANGS![2]))),
-//     (LANGS![3], include_str!(concat!("snippets/", LANGS![3]))),
-//     (LANGS![4], include_str!(concat!("snippets/", LANGS![4]))),
-//     (LANGS![5], include_str!(concat!("snippets/", LANGS![5]))),
-//     (LANGS![6], include_str!(concat!("snippets/", LANGS![6]))),
-//     (LANGS![7], include_str!(concat!("snippets/", LANGS![7]))),
-//     (LANGS![8], include_str!(concat!("snippets/", LANGS![8]))),
-//     (LANGS![9], include_str!(concat!("snippets/", LANGS![9]))),
-//     (LANGS![10], include_str!(concat!("snippets/", LANGS![10]))),
-//     (LANGS![11], include_str!(concat!("snippets/", LANGS![11]))),
-//     (LANGS![12], include_str!(concat!("snippets/", LANGS![12]))),
-//     (LANGS![13], include_str!(concat!("snippets/", LANGS![13]))),
-//     (LANGS![14], include_str!(concat!("snippets/", LANGS![14]))),
-//     (LANGS![15], include_str!(concat!("snippets/", LANGS![15]))),
-//     (LANGS![16], include_str!(concat!("snippets/", LANGS![16]))),
-//     (LANGS![17], include_str!(concat!("snippets/", LANGS![17]))),
-//     (LANGS![18], include_str!(concat!("snippets/", LANGS![18]))),
-//     (LANGS![19], include_str!(concat!("snippets/", LANGS![19]))),
-//     (LANGS![20], include_str!(concat!("snippets/", LANGS![20]))),
-//     (LANGS![21], include_str!(concat!("snippets/", LANGS![21]))),
-//     (LANGS![22], include_str!(concat!("snippets/", LANGS![22]))),
-//     (LANGS![23], include_str!(concat!("snippets/", LANGS![23]))),
-//     (LANGS![24], include_str!(concat!("snippets/", LANGS![24]))),
-//     (LANGS![25], include_str!(concat!("snippets/", LANGS![25]))),
-// ])
-//
 
 fn main() {
     println!("cargo:rerun-if-changed=src/snippets/");
@@ -108,7 +79,6 @@ fn main() {
             .join(runner.iter().skip(1).collect::<PathBuf>());
         fs::copy(runner, new_runner).unwrap();
 
-        // include!("runners/python"),
         writeln!(runners_list, "include!(\"{runner_name}\"), ",).unwrap();
     }
 
@@ -123,8 +93,12 @@ fn main() {
         .collect::<Vec<_>>()
     {
         eprintln!("alias = {alias:?}");
-        // ("yasm", LANGS!(check, "asm")),
-        writeln!(alias_map, "(\"{}\", LANGS!(check, \"{}\")),", alias[0], alias[1]).unwrap();
+        writeln!(
+            alias_map,
+            "(\"{}\", LANGS!(check, \"{}\")),",
+            alias[0], alias[1]
+        )
+        .unwrap();
     }
     alias_map.write_all(b"])\n").unwrap();
 }
