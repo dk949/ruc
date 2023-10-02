61d72d39d706623a4f5d59fbc11cf347327de138
dk949 <56653556+dk949@users.noreply.github.com>
2023-10-02 16:58:57 +0100

    fixed editor argument in nix to EDITOR from the parent shell if not specified

--- shell.nix
+++ shell.nix
@@ -1,29 +1,35 @@
 # This is not made to be 100% reproducible, just to make testing easier
 { pkgs ? import <nixpkgs> {}, editor ? ""  } :
 
-
 pkgs.mkShell {
 
     buildInputs = [
-
+        pkgs.bash
+        pkgs.clojure
+        pkgs.coffeescript
+        pkgs.dash
+        pkgs.julia
+        pkgs.lua
+        pkgs.nodejs
+        pkgs.perl
+        pkgs.php
+        pkgs.powershell
+        pkgs.python3
+        pkgs.ruby
+        pkgs.zsh
         pkgs.R
-            pkgs.bash
-            pkgs.clojure
-            pkgs.dash
-            pkgs.lua
-            pkgs.nodejs
-            pkgs.perl
-            pkgs.php
-            pkgs.powershell
-            pkgs.python3
-            pkgs.ruby
-            pkgs.zsh
 
-            pkgs.vim
-            pkgs.nano
-            pkgs.gedit
+        pkgs.vim
+        pkgs.nano
+        pkgs.gedit
     ];
 
-    EDITOR = editor
+    shellHook = ''
+        [ -n "$RUC_EDITOR" ] && {
+            echo Using "$RUC_EDITOR" as the editor
+            export EDITOR="$RUC_EDITOR"
+        }
+    '';
 
+    RUC_EDITOR = editor;
 }
